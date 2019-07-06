0e5245c0157a520df97358cfded7895c55f6c570
ljw <ljw970812@163.com>
2019-07-06 09:32:24 +0800

    start

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+      project
+  </div>
+</template>
+
+<style>
+* {margin: 0; padding: 0;}
+html, body {width: 100%; height: 100%; font-size: 0.16rem;}
+ul {list-style: none;}
+a {text-decoration: none; color: #000;}
+img {display: block;}
+h1, h2, h3 {font-size: 0.16rem; font-weight: normal;}
+.l {float: left;}
+.r {float: right;}
+.clear:after {content: "";display: block;clear: both;}
+</style>
